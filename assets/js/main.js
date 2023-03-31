b5cfc1e432272c377bfc03b321eed0e06c7d24db
Hieu Tran <Hieu.Tran@tui.co.uk>
2023-03-31 13:35:37 +0700

    update code

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,39 @@
 $(document).ready(function() {
  
   $('.owl-menu-item').owlCarousel({
-		items:5,
+		items:4,
 		loop:true,
-		dots: true,
-		nav: true,
 		autoplay: 3000,
 		margin:30,
 		  responsive:{
-        300: {
-          items: 5
+				0: {
+					items: 1
+				},
+        600: {
+					items: 2
+				},
+				1200: {
+          items: 4
         }
 		  }
 	  })
+
+		$('.owl-menu-project').owlCarousel({
+			items:4,
+			loop:true,
+			autoplay: 3000,
+			margin:30,
+				responsive:{
+					0: {
+						items: 1
+					},
+					600: {
+						items: 2
+					},
+					1200: {
+						items: 3
+					}
+				}
+			})
    
   });
